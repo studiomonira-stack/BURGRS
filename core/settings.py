a50d24564eced219440bb2959943f8dd68791b9d
studiomonira-stack <studiomonira@gmail.com>
2026-06-06 08:19:26 +0200

    Added Bosnian, Arabic and Turkish translations

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -105,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Sarajevo'
 
 USE_I18N = True
 
@@ -125,3 +127,13 @@ DEBUG = False
 ALLOWED_HOSTS = ['MoniraElCharif.pythonanywhere.com', 'www.MoniraElCharif.pythonanywhere.com']
 STATIC_ROOT = '/home/MoniraElCharif/burgrs/staticfiles'
 STATIC_URL = '/static/'
+
+LANGUAGES = [
+    ('en', _('English')),
+    ('bs', _('Bosnian')),
+    ('ar', _('Arabic')),
+]
+
+LOCALE_PATHS = [
+    BASE_DIR / 'locale',
+]
